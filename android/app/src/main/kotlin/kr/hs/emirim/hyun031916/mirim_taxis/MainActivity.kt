29dabbb154e281bdd8293be6698dacc3f88fdb80
ksh <hyun031916@gmail.com>
2020-08-09 13:43:19 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/kr/hs/emirim/hyun031916/mirim_taxis/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.hs.emirim.hyun031916.mirim_taxis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData
+        primarySwatch: Colors.blue,
+        // This makes the visual dens
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StateFulWidget{
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
